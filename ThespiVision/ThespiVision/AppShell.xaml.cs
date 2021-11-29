b8f5c16a76fc7151c047f1342b8ab401b3e7f6bf
Dandre Bell <djbell1999@comcast.net>
2021-11-29 00:50:46 -0500

    Add project files.

---
+++ ThespiVision/ThespiVision/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ThespiVision.Views;
+
+namespace ThespiVision
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(HomePage), typeof(HomePage));
+            Routing.RegisterRoute(nameof(LogInPage), typeof(LogInPage));
+            Routing.RegisterRoute(nameof(PostShowPage), typeof(PostShowPage));
+            Routing.RegisterRoute(nameof(SavedShowsPage), typeof(SavedShowsPage));
+        }
+    }
+}
---
+++ ThespiVision/ThespiVision/Views/SavedShowsPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ThespiVision.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SavedShowsPage : ContentPage
+    {
+        private ObservableCollection<string> SavedShows = new ObservableCollection<string>
+        {
+            "Show 1",
+            "Show 2",
+            "Show 3"
+        };
+
+
+
+        public SavedShowsPage()
+        {
+            InitializeComponent();
+
+            EmptySaveListText.IsVisible = !(SavedShows.Count > 0);
+
+            SavedShowListView.ItemsSource = SavedShows;
+            
+        }
+    }
+}
