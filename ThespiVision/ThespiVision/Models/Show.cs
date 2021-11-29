6e5b35b97f0c9aa1f9f33e8488179d2bd5962f7b
Dandre Bell <DJBell1999@comcast>
2021-11-29 15:11:20 -0500

    Changed access modifiers on fields

--- ThespiVision/ThespiVision/Models/Show.cs
+++ ThespiVision/ThespiVision/Models/Show.cs
@@ -7,10 +7,10 @@ namespace ThespiVision.Models
     public class Show
     {
         public string title { get; set; }
-        private string company { get; set; }
-        private string location { get; set; }
-        private DateTime showDate { get; set; }
-        private string description { get; set; }
+        public string company { get; set; }
+        public string location { get; set; }
+        public DateTime showDate { get; set; }
+        public string description { get; set; }
 
         public Show(string title, string company, string location, string description)
         {
