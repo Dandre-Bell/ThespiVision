248745a0fd9bd23ffd51568ba628435bf31698c1
Dandre Bell <DJBell1999@comcast>
2021-12-03 15:42:16 -0500

    Added slider styling and map user location centering

--- ThespiVision/ThespiVision/Views/HomePage.xaml.cs
+++ ThespiVision/ThespiVision/Views/HomePage.xaml.cs
@@ -6,15 +6,55 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+using Xamarin.Forms.Maps;
 
 namespace ThespiVision.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HomePage : ContentPage
     {
+
         public HomePage()
         {
             InitializeComponent();
+            RadiusSlider.Value = 20;
+
+            GoToUser(this, EventArgs.Empty);
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs args)
+        {
+            RadiusDisplay.Text = "Shows within " + Math.Floor(RadiusSlider.Value) + " miles";
+        }
+
+        private async void GoToUser(object sender, EventArgs args)
+        {
+            try
+            {
+                var location = await Geolocation.GetLastKnownLocationAsync();
+
+                if (location != null)
+                {
+                    showMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(location.Latitude, location.Longitude), Distance.FromMiles(1)));
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                // Handle not enabled on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+            }
         }
     }
 }
